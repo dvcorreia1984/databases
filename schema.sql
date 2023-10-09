ee8375cc6835c638357d70c04f1e76b01edfcb57
dvcorreia1984 <victor@onsleer.co.za>
2023-10-09 15:08:44 +0200

    Implemented optional changes

--- schema.sql
+++ schema.sql
@@ -65,4 +65,4 @@ ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 
    create index animal_id_desc on visits(animal_id desc);
    create index vet_id_desc on visits(vet_id desc);
-   create index email_desc on owners(email desc);
+   create index email_desc on owners((lower(email)) desc);
